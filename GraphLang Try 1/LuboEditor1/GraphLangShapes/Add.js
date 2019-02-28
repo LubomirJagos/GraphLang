155e681e515dd49678901b4e59b08472ff11035b
LubomirJagos <lubomir.jagos@gmail.com>
2019-02-28 17:58:59 +0100

    Added examples how to get nodes on canvas and call their run function to do their job and also how they can store internal persistant values.

--- GraphLang Try 1/LuboEditor1/GraphLangShapes/Add.js
+++ GraphLang Try 1/LuboEditor1/GraphLangShapes/Add.js
@@ -22,11 +22,20 @@ GraphLang.Color = Class.extend({
  */
 GraphLang.Shapes = draw2d.shape.node.Node.extend({
   NAME : "GraphLang.Shapes",
+
+  /**
+   *  This is constructor, placed here to be inherited by all child. Thanks to this I can retype Object to this class so I'm able to parse diagram and on each item retype it to some generic datatype with its run function and run it.
+   */
+  constructor(obj) {
+    obj && Object.assign(this, obj);
+  },
+  //------------------------ END OF CONSTRUCTOR-------------------------//
+
   init:function(attr, setter, getter){
     this._super($.extend({stroke:0,bgColor:GraphLang.Color.getByName('yellow'),width:210,height:179},attr), setter, getter);
   },
   onRun: function(){
-    alert(this.id); //show ID by default everytime it's hitted by heartbeat
+    alert(this.id) //default everytime it's hitted by heartbeat
   }
 });
 
@@ -52,7 +61,16 @@ GraphLang.Shapes.Numeric.Add = draw2d.shape.node.End.extend({
   init:function(attr, setter, getter){
     this._super($.extend(attr,{color: "#004200", bgColor: "#FFFF00", width: "42", height: "42"}), setter, getter);
   },
-  onRun: function(attr){
-    alert(this.id);
+  onRun: function(){
+    this.cnt1++;
+    alert(this.id + "\n" + " -----> " + String(this.cnt1));
+  },
+
+  /**
+   *  This function takes caller object as parameter, to be able modify instance parameters.
+   */
+  onRun2: function(caller){
+    caller.cnt1++;
+    alert(caller.id + "\n" + " -----> " + String(caller.cnt1));
   }
 });
